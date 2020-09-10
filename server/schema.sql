77d5f6a44c9c04e73105e272396c6d6d82428da2
Katie Poa <kpoa@berkeley.edu>
2020-09-09 17:45:59 -0700

    Create messages and users tables in schema.sql, fix server-spec.js according to our schema, copy over chatterbox-client

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,32 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  txt VARCHAR(20) NOT NULL,
+  user_id INT,
+  roomname VARCHAR(20)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20) NOT NULL
+);
 
+-- possibly rooms?
 
-
+/* Create a MySQL Database From the Interactive Prompt
+From the interactive prompt, CREATE and USE a new database
+Create a new table with at least 3 columns, one of them set as a primary key
+Use the DESCRIBE <table-name> command to verify the setup of your new table
+INSERT some new rows into your new table
+Execute several queries
+Use UPDATE key word in at least 2 different ways
+ Exit the interactive prompt */
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
